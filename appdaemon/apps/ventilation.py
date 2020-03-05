6c745706fbbe2037148841599eb8bae5a56e88bb
benny0708 <32078093+benny0708@users.noreply.github.com>
2020-03-05 22:38:43 +0100

    updated ventilation
    delay eingeführt
    auf groß und kleinschreibung geachtet ;)
    variablen für Low Med High

--- appdaemon/apps/ventilation.py
+++ appdaemon/apps/ventilation.py
@@ -12,23 +12,30 @@ class VentilationWorld(hass.Hass):
 
     def initialize(self):
         self.log("Starting Ventilation Service")
-        self.listen_state(self.ventilation_selector_switch,"binary_sensor.lueftung_low")
-        self.listen_state(self.ventilation_selector_switch,"binary_sensor.lueftung_medium")
-        self.listen_state(self.ventilation_selector_switch,"binary_sensor.lueftung_high")
+        self.listen_state(self.delay_ventilation_selector_switch,"binary_sensor.lueftung_low")
+        self.listen_state(self.delay_ventilation_selector_switch,"binary_sensor.lueftung_medium")
+        self.listen_state(self.delay_ventilation_selector_switch,"binary_sensor.lueftung_high")
 
-    def ventilation_selector_switch(self, entity, attribute, old, new,kwargs):
-        self.log("Ventilation Selector Switch is operated")
-        if(self.get_state("binary_sensor.lueftung_low") == "Off" and self.get_state("binary_sensor.lueftung_medium") == "Off" and self.get_state("binary_sensor.lueftung_high") == "Off"):
+    def ventilation_selector_switch(self, entity):
+        self.log("Waiting time finished")
+        VentLow = self.get_state("binary_sensor.lueftung_low")
+        VentMed = self.get_state("binary_sensor.lueftung_medium")
+        VentHigh = self.get_state("binary_sensor.lueftung_high")
+        if(VentLow == "off" and VentMed == "off" and VentHigh == "off"):
             self.log("Ventilation Off")
             self.call_service("fan/set_speed", entity_id="fan.lueftung", speed="off")
-        elif(self.get_state("binary_sensor.lueftung_low") == "On" and self.get_state("binary_sensor.lueftung_medium") == "Off" and self.get_state("binary_sensor.lueftung_high") == "Off"):
+        elif(VentLow == "on" and VentMed == "off" and VentHigh == "off"):
             self.log("Ventilation Speed set to Low")
             self.call_service("fan/set_speed", entity_id="fan.lueftung", speed="low")
-        elif(self.get_state("binary_sensor.lueftung_low") == "Off" and self.get_state("binary_sensor.lueftung_medium") == "On" and self.get_state("binary_sensor.lueftung_high") == "Off"):
+        elif(VentLow == "off" and VentMed == "on" and VentHigh == "off"):
             self.log("Ventilation Speed set to Medium")
             self.call_service("fan/set_speed", entity_id="fan.lueftung", speed="medium")
-        elif(self.get_state("binary_sensor.lueftung_low") == "Off" and self.get_state("binary_sensor.lueftung_medium") == "Off" and self.get_state("binary_sensor.lueftung_high") == "On"):
+        elif(VentLow == "off" and VentMed == "off" and VentHigh == "on"):
             self.log("Ventilation Speed set to High")
             self.call_service("fan/set_speed", entity_id="fan.lueftung", speed="high")
         else:
             self.log("Error operating selector switch: No position detected")
+
+    def delay_ventilation_selector_switch(self, entity, attribute, old, new,kwargs):
+        self.log("Ventilation Selector Switch is operated")
+        self.run_in(self.ventilation_selector_switch, 1)
